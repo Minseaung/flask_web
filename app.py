fa830f6e3fcc229edd34db5a2256cfc1aa5a490e
lalala <lalala@naver.com>
2021-04-19 17:27:31 +0900

    params 활용까지

--- app.py
+++ app.py
@@ -8,22 +8,24 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def index():
     #return "Hello World"
-    return render_template("index.html", data = 'KIM')
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
-    return render_template("about.html", hello = "Gary Kim")
+    return render_template("about.html")
 
-@app.route('/articles')
+@app.route('/articles') #사이트 뒤에 /공간 을 지정해주는 것
 def articles():
-    articles = Articles()
+    articles = Articles() #Articles()는 data.py에서 받아옴! 그 함수를 변수로 지정해주는 정보지정.
     # print(articles[0]['title'])
-    return render_template("articles.html", articles = articles)
+    return render_template("articles.html", articles = articles) #articles.html = 공간. 사이트가 이동했을 때 존재하는 공간, 즉 바구니를 지정해줌. 변수 = articles는 위에서 지정한 정보를 입력해주는 용도. 없으면 안됨!
 
-@app.route('/article/<id>')
+@app.route('/article/<int:id>')
 def article(id):
-    print(id)
-    return "Success"
+    articles = Articles()
+    article = articles[id-1] #딕셔너리에서 가장 첫 값은 0인데 지정으로 불러오면 1이 되기 때문에 0을 만들기 위해 -1을 붙여줌
+    print(articles[id-1])
+    return render_template("article.html", article = article)
 
 if __name__ == '__main__':
     app.run()
